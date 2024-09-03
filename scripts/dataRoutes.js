2f4652ddf387fb8f007b0bec478c2bdfe32b0e88
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-09-03 19:45:54 +0200

    Create dataRoutes.js

---
+++ scripts/dataRoutes.js
@@ -0,0 +1,110 @@
+const routes = [
+    {
+        name: "Héroes",
+        images: "",
+        level: ["medio", "alto"],
+        approximateDistance: "",
+        description: "Ruta muy disfrutable, con muchos kilómetros de suave bajada, hasta llegar a cuesta de la vega, donde la cosa se pone interesante.",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    },
+    {
+        name: "",
+        images: "",
+        level: "",
+        approximateDistance: "",
+        description: "",
+        map: "",
+        reviews:""
+    }
+]
