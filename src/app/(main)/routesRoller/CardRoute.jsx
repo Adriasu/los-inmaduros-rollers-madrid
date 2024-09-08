6719b046e4506bc1b710744310d7b98e69976f48
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-09-08 16:50:09 +0200

    Update CardRoute.jsx

--- src/app/(main)/routesRoller/CardRoute.jsx
+++ src/app/(main)/routesRoller/CardRoute.jsx
@@ -3,9 +3,13 @@ import React from "react";
 
 const CardRoute = ({ route }) => {
   return (
-    <div>
+    <div className="flex flex-col w-[220px] h-[350px] border border-black rounded-lg p-2">
       <div className="overflow-hidden h-[200px] flex justify-center items-center border border-black rounded-lg bg-slate-300">
         <Image src={route.image} alt={route.name} width={200} height={300} />
+      </div>
+      <h2>{route.name}</h2>
+      <div>
+        
       </div>
 
     </div>
