c4c869ac37b5b1fd6e4cd4a39ff153957bce9ce3
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-09-11 14:05:09 +0200

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Menu } from "lucide-react";
 import React from "react";
 import { Button } from 'primereact/button';
+import Image from "next/image";
 
 
 const Navbar = () => {
   return (
-    <div className="h-14 w-full bg-slate-600 flex items-center justify-between py-2 px-3">
+    <div className="h-14 w-full bg-[#5ec8e6] flex items-center justify-between py-2 px-3">
       <Menu />
-      <h2>LOGO</h2>
+      <Image src={"/images/logo1.png"} alt="Los inmaduros roller Madrid" width={130} height={50} className="rounded"/>
       <Button label="login" severity="info" rounded className="px-3 py-1"/>
     </div>
   );
