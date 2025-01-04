aaa398f6d79ac0bf48f8d1a0e82f882f8da0e8c6
Adriasu <167806166+Adriasu@users.noreply.github.com>
2025-01-04 20:44:29 +0100

    Update HomeBanner.jsx

--- src/app/(main)/HomeBanner.jsx
+++ src/app/(main)/HomeBanner.jsx
@@ -13,7 +13,7 @@ const HomeBanner = () => {
       playsInline
       width="100%"
     ></video>
-    <div className="absolute left-[21px] bottom-5 md:left-10 md:bottom-[50px]">
+    <div className="absolute left-[15px] bottom-5 md:left-10 md:bottom-[50px]">
       <h1 className="text-xl font-bold sm:text-3xl bg-gradient-to-r from-cyan-600 to-cyan-200 bg-clip-text text-transparent">¡Rueda con nosotros por Madrid!</h1>
       <p className="text-body13 text-white mt-2 tracking-[-0.004em] md:hidden">La comunidad que reinventa la forma <br /> de explorar la ciudad. <br /> Crea tu propia ruta y comparte.</p>
       <p className="hidden md:flex text-body13 mt-4 text-white">La comunidad que reinventa la forma de explorar la ciudad. <br /> Crea tu propia ruta y comparte.</p>
